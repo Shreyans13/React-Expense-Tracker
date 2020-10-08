a3fb27553db31b29a608c70dcf96a6f21cd1fd26
Shreyans13 <shreyans1313@gmail.com>
2020-10-08 15:25:32 +0530

    Updates

---
+++ precache-manifest.900d8cdc578ee3f45de4302951e32fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8663c25da6a87748b3791be44a295db1",
+    "url": "/React-Expense-Tracker%20/index.html"
+  },
+  {
+    "revision": "e467dcb6a74c45e88de6",
+    "url": "/React-Expense-Tracker%20/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/React-Expense-Tracker%20/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Expense-Tracker%20/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e467dcb6a74c45e88de6",
+    "url": "/React-Expense-Tracker%20/static/js/main.a1c3d08c.chunk.js"
+  },
+  {
+    "revision": "30a48e334cd1dbf01d2d",
+    "url": "/React-Expense-Tracker%20/static/js/runtime-main.6a9768c8.js"
+  }
+]);
